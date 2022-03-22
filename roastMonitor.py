4c470e44e77614a147b7161ab0ed5490306b8ea4
unknown <bdamore916@gmail.com>
2022-03-22 13:29:57 -0500

    added an escape char

--- roastMonitor.py
+++ roastMonitor.py
@@ -21,7 +21,7 @@ sheet=client.open(ssName).worksheet(wsName)
 
 # Create the search for the weight, title, and blend component
 # batchSearch = re.compile(r"'weightin': (\d+)")
-batchSearch = re.compile(r"'weight': [(\d+).")
+batchSearch = re.compile(r"'weight': \[(\d+).")
 coffeeSearch = re.compile(r"'title': '([a-zA-Z ]+)(\d+)?',")
 blendSearch = re.compile(r"'beans': '([a-zA-Z ]+)',")
 roasterSearch = re.compile(r"'operator': '([a-zA-Z]+)'")
